a63ef13400054886b5595c3edac1ab7ebe6df194
Andrew Froiland <afroiland@Andrews-MacBook-Pro.local>
2016-11-13 18:40:22 -0600

    trying to fix listen port

--- server/app.js
+++ server/app.js
@@ -16,6 +16,4 @@ app.use('/', index);
 
 app.set('port', process.env.PORT || 3000);
 
-app.listen(app.get('port'), function() {
-  console.log('server is listening on port ' + app.get('port'));
-});
+app.listen(process.env.PORT || 5000)
